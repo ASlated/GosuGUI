58caa4fd3f91b7e8d7d3105c4d5e53583676f52d
Aidan Slate <slate.aidan@gmail.com>
2017-07-04 17:05:24 -0400

    slider2: retrieving value redone, now symbols, no longer options hash

--- run.rb
+++ run.rb
@@ -10,7 +10,7 @@ class GameWindow < Gosu::Window
     super(720, 480)
     self.caption = 'Input'
     @slider = Slider.new(self, 10, 40, 300, 50, 0.5)
-    @slider_2 = SliderAlt.new(self, 50, 300, 0, 400, 50)
+    @slider_2 = SliderAlt.new(self, 50, 300, 0, 400, 10000)
     @font = Gosu::Font.new(16, {name: 'default'})
   end
 
@@ -32,6 +32,7 @@ class GameWindow < Gosu::Window
       @slider.instance_variable_set(:@dragging, false)
     end
     @slider.dragging(mouse_x)
+    @slider_2.update
   end
 
   def draw
--- slider_2.rb
+++ slider_2.rb
@@ -10,47 +10,70 @@ class SliderAlt
   C = Gosu::Color.rgb(128, 128, 128)
   FONT_SIZE = 20
 
-  def initialize(window, x, y, z, length, max, options = {min: 0, pos: 0.0, tick_marks: 0, color: C})
+  def initialize(window, x, y, z, length, max, min: 0, pos: 0.0, markers: 0, color: C, text: true)
     @window = window
     @x, @y, @z = x, y, z
     @length = length
-    @max = max
-    @options = options
-    @value = @options[:pos] * @max
+    @max, @min = max, min
+    @pos = pos
+    @markers = markers
+    @color = color
+    @text = text
     @dragging = false
     @font = Gosu::Font.new(FONT_SIZE, {name: 'slider_font'})
-    @pointerx = @x + @options[:pos] * @length - PWIDTH / 2
-    @poxntery = @y + MARGIN + LHEIGHT
+    @pointerx = @x + @pos * @length - PWIDTH / 2
+    @pointery = @y + MARGIN + LHEIGHT
+    @dir = 'up'
+  end
+
+  def value
+    (@max - @min) * @pos + @min
+  end
+
+  def max
+    @max
   end
 
   def update
-    @options[:pos] = rand(10) / 10.0
-    @value = @options[:pos] * @max
-    @pointerx = @x + @options[:pos] * @length - PWIDTH / 2
-    @poxntery = @y + MARGIN + LHEIGHT
+    if @dir == 'up'
+      unless @pos >= 1
+        @pos += 0.01
+      else
+        @dir = 'down'
+        @pos -= 0.01
+      end
+    else
+      unless @pos <= 0
+        @pos -= 0.01
+      else
+        @dir = 'up'
+        @pos += 0.01
+      end
+    end
+    @pointerx = @x + @pos * @length - PWIDTH / 2
+    @pointery = @y + MARGIN + LHEIGHT
   end
 
   def draw_pointer
-    x = @pointerx
-    y = @pointery
-    @window.draw_quad(x, y - PHEIGHT, @options[:color], x + PWIDTH, y - PHEIGHT, @options[:color], x, y - THEIGHT, @options[:color], x + PWIDTH, y - THEIGHT, @options[:color], @z)
-    @window.draw_triangle(x, y - THEIGHT, @options[:color], x + PWIDTH / 2, y, @options[:color], x + PWIDTH, y - THEIGHT, @options[:color], @z)
+    x, y = @pointerx, @pointery
+    @window.draw_quad(x, y - PHEIGHT, @color, x + PWIDTH, y - PHEIGHT, @color, x, y - THEIGHT, @color, x + PWIDTH, y - THEIGHT, @color, @z)
+    @window.draw_triangle(x, y - THEIGHT, @color, x + PWIDTH / 2, y, @color, x + PWIDTH, y - THEIGHT, @color, @z)
   end
 
   def draw_bar
-    @window.draw_quad(@x, @y, @options[:color], @x + @length, @y, @options[:color], @x, @y + LHEIGHT, @options[:color], @x + @length, @y + LHEIGHT, @options[:color], @z)
+    @window.draw_quad(@x, @y, @color, @x + @length, @y, @color, @x, @y + LHEIGHT, @color, @x + @length, @y + LHEIGHT, @color, @z)
   end
 
   def draw_text
-    @font.draw(@options[:min].round, @x, @y + LHEIGHT + MARGIN, @z, 1, 1, @options[:color])
-    @font.draw_rel(@max.round, @x + @length, @y + LHEIGHT + MARGIN, @z, 1, 0, 1, 1, @options[:color])
-    @font.draw_rel(@value.round, @x + @length, @y - MARGIN - QHEIGHT, @z, 1, 1, 1, 1, @options[:color])
+    @font.draw(@min.round, @x, @y + LHEIGHT + MARGIN, @z, 1, 1, @color)
+    @font.draw_rel(@max.round, @x + @length, @y + LHEIGHT + MARGIN, @z, 1, 0, 1, 1, @color)
+    @font.draw_rel(value.round, @x + @length, @y - MARGIN - QHEIGHT, @z, 1, 1, 1, 1, @color)
   end
 
-  def draw(text = true)
+  def draw
     draw_pointer
     draw_bar
-    draw_text if text
+    draw_text if @text
   end
 
   def check_mouse(mouse_x, mouse_y, x1, y1, x2, y2)
